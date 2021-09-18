fa9aabfdf2c2f62a3ac4ca12ba40b9320fbe8253
George Sukhadolsky <sukhadol@yandex.ru>
2021-09-18 10:28:03 +0300

    Update RSS_from_FB.py

--- RSS_from_FB.py
+++ RSS_from_FB.py
@@ -242,8 +242,9 @@ def read_article_feed(feed):
             if article_NOT_in_BazeFromRSS(article['title'], article['published']):
                 add_article_to_db(article['title'], article['published'])
                 #bot_sendtext('*Форвард нового сообщения из Фейсбука:*\n\n' + text_of_article + article['link']) #эта строка была сокращенной версией, без учета излишне линных сообщений
-                full_text = r'*Форвард нового сообщения из Фейсбука:*\n\n' + text_of_article + article['link']
-                #full_text = full_text.replace("#", " :")
+                #full_text = '*Форвард нового сообщения из Фейсбука:*\n\n' + r + text_of_article + article['link']
+                #full_text = r'*Форвард нового сообщения из Фейсбука:*\n\n' + text_of_article + article['link']
+                full_text = full_text.replace("#", " %23")
                 # шестнадцатеричный код символа # = 0023, т.е. для отображения '\x23'. Но при его подстановке - в bot_sendtext вся последующая строка рассматривается как примечание и не видна
                 if len(full_text) > 4096:
                     full_text_fix= len(full_text)
