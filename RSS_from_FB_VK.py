9b7c8e6dac9d7bc044c7e2297e5431efa8c9413b
George Sukhadolsky <sukhadol@yandex.ru>
2021-09-28 19:07:51 +0300

    Update RSS_from_FB_VK.py

--- RSS_from_FB_VK.py
+++ RSS_from_FB_VK.py
@@ -383,9 +383,9 @@ def read_article_feed(feed):
 
             if article_NOT_in_BazeFromRSS(article['title'], article['published']):
                 add_article_to_db_from_FB(article['title'], article['published'])
-                if(len(text_of_article)) > 2500: # без этого ограничения выдавало ошибку 414 Request-URI Too Large
-                    #txt_tmp = text_of_article[: 2500] + '...(есть продолжение)'
-                    bot_sendtext_to_VK_from_FB('Форвард нового сообщения из Фейсбука:\n\n' + text_of_article[: 2000] + '...\nПродолжение в источнике:\n' + article['link']) #эта строка была сокращенной версией, без учета излишне длинных сообщений
+                if(len(text_of_article)) > 3000: # без этого ограничения выдавало ошибку 414 Request-URI Too Large
+                    #txt_tmp = text_of_article[: 3000] + '...(есть продолжение)'
+                    bot_sendtext_to_VK_from_FB('Форвард нового сообщения из Фейсбука:\n\n' + text_of_article[: 2000] + '...\n\nПродолжение в источнике:\n' + article['link']) #эта строка была сокращенной версией, без учета излишне длинных сообщений
                 else:
                     bot_sendtext_to_VK_from_FB('Форвард нового сообщения из Фейсбука:\n\n' + text_of_article + article['link']) #эта строка была сокращенной версией, без учета излишне длинных сообщений
                 full_text = '*Форвард нового сообщения из Фейсбука:*\n\n' + text_of_article + article['link']
@@ -408,7 +408,7 @@ def read_article_feed(feed):
                 print(article['title'])
             else:
                 print('...добавлять и публиковать данный пост не надо, уже есть, речь о посте=')
-                print(article['title'])
+                print(article['title'][: 100]) #публикуем только первые 100 символов
 #               print(text_of_article)
     except (Exception, Error) as error:
         print("Какая-то ошибка - 364: ", error)
